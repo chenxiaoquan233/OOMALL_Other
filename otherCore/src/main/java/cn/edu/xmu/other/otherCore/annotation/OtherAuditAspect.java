206df880c7dcd33fa826a3b12e99c9bcb9ce109b
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-11-25 10:55:21 +0800

    finish first version otherCore

---
+++ otherCore/src/main/java/cn/edu/xmu/other/otherCore/annotation/OtherAuditAspect.java
@@ -0,0 +1,101 @@
+package cn.edu.xmu.other.otherCore.annotation;
+
+import cn.edu.xmu.ooad.annotation.LoginUser;
+import cn.edu.xmu.ooad.util.JwtHelper;
+import cn.edu.xmu.ooad.util.ResponseCode;
+import cn.edu.xmu.ooad.util.ResponseUtil;
+import cn.edu.xmu.other.otherCore.util.OtherJwtHelper;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+/**
+ * @author XQChen
+ * @version 创建时间：2020/11/25 上午10:44
+ */
+@Aspect
+@Component
+public class OtherAuditAspect {
+    //注入Service用于把日志保存数据库
+
+    private  static  final Logger logger = LoggerFactory.getLogger(OtherAuditAspect. class);
+
+    //Controller层切点
+    @Pointcut("@annotation(cn.edu.xmu.other.otherCore.annotation.Audit)")
+    public void auditAspect() {
+    }
+
+    /**
+     * 前置通知 用于拦截Controller层记录用户的操作
+     *
+     * @param joinPoint 切点
+     */
+    @Before("auditAspect()")
+    public void doBefore(JoinPoint joinPoint) {
+    }
+
+    //配置controller环绕通知,使用在方法aspect()上注册的切入点
+    @Around("auditAspect()")
+    public Object around(JoinPoint joinPoint){
+        logger.debug("around: begin joinPoint = "+ joinPoint);
+        MethodSignature ms = (MethodSignature) joinPoint.getSignature();
+        Method method = ms.getMethod();
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+        String token = request.getHeader(JwtHelper.LOGIN_TOKEN_KEY);
+        if (token == null){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return ResponseUtil.fail(ResponseCode.AUTH_NEED_LOGIN);
+        }
+
+        Long userId = new OtherJwtHelper().verifyTokenAndGetClaims(token);
+
+        logger.debug("around: userId =" + userId);
+        if (userId == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return ResponseUtil.fail(ResponseCode.AUTH_NEED_LOGIN);
+        }
+
+        Object[] args = joinPoint.getArgs();
+        Annotation[][] annotations = method.getParameterAnnotations();
+        for (int i = 0; i < annotations.length; i++) {
+            Object param = args[i];
+            Annotation[] paramAnn = annotations[i];
+            if (paramAnn.length == 0){
+                continue;
+            }
+
+            for (Annotation annotation : paramAnn) {
+                //这里判断当前注解是否为LoginUser.class
+                if (annotation.annotationType().equals(LoginUser.class)) {
+                    //校验该参数，验证一次退出该注解
+                    args[i] = userId;
+                }
+            }
+        }
+
+        Object obj = null;
+        try {
+
+            obj = ((ProceedingJoinPoint) joinPoint).proceed(args);
+        } catch (Throwable e) {
+
+        }
+        return obj;
+    }
+}
