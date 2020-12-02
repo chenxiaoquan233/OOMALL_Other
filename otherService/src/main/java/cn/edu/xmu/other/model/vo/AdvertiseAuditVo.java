dcf112645152cf30469460e2d0de17d5f08a6981
hardworking-qf <1183364194@qq.com>
2020-12-02 17:58:07 +0800

    AdAudit不知道怎么createBo

--- otherService/src/main/java/cn/edu/xmu/other/model/vo/AdvertiseAuditVo.java
+++ otherService/src/main/java/cn/edu/xmu/other/model/vo/AdvertiseAuditVo.java
@@ -1,7 +1,10 @@
 package cn.edu.xmu.other.model.vo;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @author hardworking-qf
@@ -10,5 +13,11 @@ import lombok.Data;
 @Data
 @ApiModel
 public class AdvertiseAuditVo {
+    @NotNull(message = "结果不能为空")
+    @ApiModelProperty(name = "conclusion", value = "true")
+    private Boolean conclusion;
 
+    @NotNull(message = "信息不能为空")
+    @ApiModelProperty(name = "message", value = "message")
+    private String message;
 }
