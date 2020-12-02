d0be1e6bb953c26d962c368b3052ad74934968c1
hardworking-qf <1183364194@qq.com>
2020-12-02 19:02:22 +0800

    Add Shares

---
+++ otherService/src/main/java/cn/edu/xmu/other/model/vo/ShareActivityRetVo.java
@@ -0,0 +1,19 @@
+package cn.edu.xmu.other.model.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author hardworking-qf
+ * @version 创建时间：2020/12/2 下午6:04
+ */
+@Data
+public class ShareActivityRetVo {
+    private Long id;
+    private Long shopId;
+    private Long goodSpuId;
+    private LocalDateTime beginTime;
+    private LocalDateTime endTime;
+    private Integer state;
+}
---
+++ otherService/src/main/java/cn/edu/xmu/other/model/vo/SharesVo.java
@@ -0,0 +1,31 @@
+package cn.edu.xmu.other.model.vo;
+
+import cn.edu.xmu.other.model.bo.SharesBo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author hardworking-qf
+ * @version 创建时间：2020/12/2 下午6:01
+ */
+@Data
+@ApiModel
+public class SharesVo {
+    @NotNull(message = "分享者Id不能为空")
+    @ApiModelProperty(name = "shareId", value = "0")
+    private Long sharerId;
+
+    @NotNull(message = "商品SpuId不能为空")
+    @ApiModelProperty(name = "goodsSpuId", value = "0")
+    private Long goodSpuId;
+
+    public SharesBo createBo(){
+        SharesBo sharesBo = new SharesBo();
+        sharesBo.setSharerId(this.sharerId);
+        sharesBo.setGoodsSpuId(this.goodSpuId);
+        return sharesBo;
+    }
+}
