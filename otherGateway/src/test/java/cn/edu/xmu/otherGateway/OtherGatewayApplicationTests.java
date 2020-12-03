84019176000deb9952caeedc7a8268eb65fa98f7
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-12-03 22:29:35 +0800

    添加otherGateway模块

---
+++ otherGateway/src/test/java/cn/edu/xmu/otherGateway/OtherGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.otherGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
