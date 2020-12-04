85d439681f0d7fbdbe36161dcad6caecd8c5c4ed
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-12-04 22:30:15 +0800

    1.delete otherOrder module (merge into otherService) 2.rename otherOrder-client to otherOrderInterface 3.make otherService depend on otherOrderInterface

---
+++ otherOrderInterface/src/main/java/cn/edu/xmu/oomall/other/service/IShareService.java
@@ -0,0 +1,25 @@
+package cn.edu.xmu.oomall.other.service;
+
+import cn.edu.xmu.oomall.other.dto.OrderItem;
+
+/**
+ * @author xincong yao
+ * @date 2020-11-17
+ */
+public interface IShareService {
+
+	/**
+	 * 发送分享成功的消息
+	 * 其他模块提供接口还是发消息队列，待决定
+	 * @param orderItem
+	 */
+	void sendShareMessage(OrderItem orderItem);
+
+	/**
+	 * 获取点击记录
+	 * @param customerId
+	 * @param skuId
+	 * @return
+	 */
+	Long getBeSharedId(Long customerId, Long skuId);
+}
