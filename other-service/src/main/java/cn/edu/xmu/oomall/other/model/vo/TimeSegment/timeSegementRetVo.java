8345bdd60dad69434fc77adcf7e6f4c21e890b24
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-12-09 21:08:34 +0800

    add time VOs

---
+++ other-service/src/main/java/cn/edu/xmu/oomall/other/model/vo/TimeSegment/timeSegementRetVo.java
@@ -0,0 +1,22 @@
+package cn.edu.xmu.oomall.other.model.vo.TimeSegment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author XQChen
+ * @version 创建时间：2020/12/9 下午8:54
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class timeSegementRetVo {
+    private Integer id;
+    private LocalDateTime beginTime;
+    private LocalDateTime endTime;
+    private LocalDateTime gmtCreate;
+    private LocalDateTime gmtModified;
+}
