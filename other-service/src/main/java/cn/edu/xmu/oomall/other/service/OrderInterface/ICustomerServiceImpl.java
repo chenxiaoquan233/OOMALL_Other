b3ce95385bae07a1ddcbf96ee1399e6caa420d7b
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-12-12 20:32:02 +0800

    update order interface

--- other-service/src/main/java/cn/edu/xmu/oomall/other/service/OrderInterface/ICustomerServiceImpl.java
+++ other-service/src/main/java/cn/edu/xmu/oomall/other/service/OrderInterface/ICustomerServiceImpl.java
@@ -1,9 +1,9 @@
 package cn.edu.xmu.oomall.other.service.OrderInterface;
 
 import cn.edu.xmu.oomall.other.dao.UserDao;
-import cn.edu.xmu.oomall.dto.CustomerDTO;
+import cn.edu.xmu.oomall.other.dto.CustomerDTO;
 import cn.edu.xmu.oomall.other.model.bo.UserBo;
-import cn.edu.xmu.oomall.impl.ICustomerService;
+import cn.edu.xmu.oomall.other.impl.ICustomerService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author XQChen
  * @version 创建时间：2020/12/4 下午10:47
  */
-@DubboService(registry = {"provider1"})
+@DubboService(registry = {"provider1"}, version = "0.0.1-SNAPSHOT")
 public class ICustomerServiceImpl implements ICustomerService {
     @Autowired
     private UserDao userDao;
