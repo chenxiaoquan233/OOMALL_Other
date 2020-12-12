7863682fcfa088899a9d733f5d9772fc538e1436
yanjiqiang <24320172203229@stu.xmu.edu.cn>
2020-12-12 20:30:43 +0800

    --story=1003015 --user=两吉 与商品模块通讯服务 点击商品后生成beshare 对象 同时返回该beshare对象是否创建成功 https://www.tapd.cn/54028298/s/1000840

---
+++ other-service/src/main/java/cn/edu/xmu/oomall/other/service/GoodsInterface/IBeShareServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.edu.xmu.oomall.other.service.GoodsInterface;
+
+import cn.edu.xmu.oomall.impl.IBeShareService;
+
+/**
+ * 2 * @author: LiangJi3229
+ * 3 * @date: 2020/12/12 下午8:28
+ * 4
+ */
+public class IBeShareServiceImpl implements IBeShareService {
+    @Override
+    public boolean createBeShare(Long customerId, Long shareId, Long skuId) {
+        return false;
+    }
+}
