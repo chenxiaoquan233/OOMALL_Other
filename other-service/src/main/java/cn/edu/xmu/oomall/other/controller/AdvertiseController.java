55cdae13aaf12887bb6488288c84595c0c97bb98
hardworking-qf <1183364194@qq.com>
2020-12-14 11:13:05 +0800

    Modify Adv, 1 APIs left wo really buhui write redis

--- other-service/src/main/java/cn/edu/xmu/oomall/other/controller/AdvertiseController.java
+++ other-service/src/main/java/cn/edu/xmu/oomall/other/controller/AdvertiseController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author hardworking-qf
@@ -115,7 +117,10 @@ public class AdvertiseController {
     })
     @PostMapping("/shops/{did}/advertisement/{id}/uploadImg")
     public Object uploadAdvertisementImgById(@LoginUser Long user, @PathVariable("id") Integer id, @RequestParam("img")MultipartFile multipartFile){
-        return null;
+        ResponseCode responseCode=advertiseService.uploadAdvertiseImgById(id,multipartFile);
+        if(responseCode.equals(ResponseCode.OK))
+            return ResponseUtil.ok();
+        else return ResponseUtil.fail(responseCode);
     }
 
 
@@ -131,7 +136,10 @@ public class AdvertiseController {
     })
     @PutMapping("/shops/{did}/advertisement/{id}/onshelves")
     public Object onshelvesAdvertisementById(@LoginUser Long user, @PathVariable("id") Integer id){
-        return null;
+        ResponseCode responseCode=advertiseService.onshelvesAdvertisementById(id);
+        if(responseCode.equals(ResponseCode.OK))
+            return ResponseUtil.ok();
+        else return ResponseUtil.fail(responseCode);
     }
 
 
@@ -146,7 +154,10 @@ public class AdvertiseController {
     })
     @PutMapping("/shops/{did}/advertisement/{id}/offshelves")
     public Object offshelvesAdvertisementById(@LoginUser Long user, @PathVariable("id") Integer id){
-        return null;
+        ResponseCode responseCode=advertiseService.offshelvesAdvertisementById(id);
+        if(responseCode.equals(ResponseCode.OK))
+            return ResponseUtil.ok();
+        else return ResponseUtil.fail(responseCode);
     }
 
 
@@ -161,14 +172,20 @@ public class AdvertiseController {
     })
     @PutMapping("/shops/{did}/advertisement/{id}/audit")
     public Object auditAdvertisementById(@LoginUser Long user, @PathVariable("id") Integer id){
-        return null;
+        ResponseCode responseCode=advertiseService.auditAdvertisementById(id);
+        if(responseCode.equals(ResponseCode.OK))
+            return ResponseUtil.ok();
+        else return ResponseUtil.fail(responseCode);
     }
 
 
     @ApiOperation(value = "管理员查看某一个广告时间段的广告", produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "header", dataType = "String", name = "authorization", value = "用户token", required = true),
-            @ApiImplicitParam(paramType = "path", dataType = "String", name = "id", value = "广告id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "did", value = "店ID", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "id", value = "广告id", required = true),
+            @ApiImplicitParam(paramType = "query", dataType = "String", name = "beginDate", value = "广告开始日期"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", name = "endDate", value = "广告结束日期"),
             @ApiImplicitParam(paramType = "query", dataType = "Integer", name = "page", value = "页码"),
             @ApiImplicitParam(paramType = "query", dataType = "Integer", name = "pageSize", value = "每页数目")
     })
@@ -176,15 +193,27 @@ public class AdvertiseController {
             @ApiResponse(code = 0,   message = "成功")
     })
     @GetMapping("/shops/{did}/timesegments/{id}/advertisement")
-    public Object getAdvertisementsByTimeSegmentId(@LoginUser Long user, @PathVariable("id") Integer id, @RequestParam(required = false) Integer page, @RequestParam(required = false) Integer pageSize){
-        return null;
+    public Object getAdvertisementsByTimeSegmentId(@LoginUser Long user,@PathVariable("did") Integer did, @PathVariable("id") Integer id,
+                                                   @RequestParam(required = false) String beginDate, @RequestParam(required = false) String endDate,
+                                                   @RequestParam(required = false) Integer page, @RequestParam(required = false) Integer pageSize){
+        // 进来的query是String的LocalDate我贞德找不到例子了QAQ
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate beginDate_,endDate_;
+        try {
+            beginDate_ = LocalDate.parse(beginDate, dateFormatter);
+            endDate_ = LocalDate.parse(endDate,dateFormatter);
+        }catch (Exception e){
+            return ResponseUtil.fail(ResponseCode.INTERNAL_SERVER_ERR);
+        }
+        return advertiseService.getAdvertiseByTimeSegmentId(id,beginDate_,endDate_,page,pageSize);
     }
 
 
     @ApiOperation(value = "管理员在广告时段下新建广告", produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "header", dataType = "String", name = "authorization", value = "用户token", required = true),
-            @ApiImplicitParam(paramType = "path", dataType = "String", name = "id", value = "广告id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "did", value = "店ID", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "id", value = "广告id", required = true),
             @ApiImplicitParam(paramType = "body",   dataType = "AdvertiseVo", name = "vo",            value = "可修改的广告信息",  required = true)
 
     })
@@ -193,16 +222,20 @@ public class AdvertiseController {
             @ApiResponse(code=603, message="达到时段广告上限")
     })
     @PostMapping("/shops/{did}/timesegments/{id}/advertisement")
-    public Object createAdvertisementsByTimeSegmentId(@LoginUser Long user, @PathVariable("id") Integer id, @RequestBody AdvertiseVo advertiseVo){
-        return null;
+    public Object createAdvertisementsByTimeSegmentId(@LoginUser Long user,  @PathVariable("did") Integer did, @PathVariable("id") Integer id, @RequestBody AdvertiseVo advertiseVo){
+        ResponseCode responseCode=advertiseService.createAdvertiseByTimeSegId(id,advertiseVo);
+        if(responseCode.equals(ResponseCode.OK))
+            return ResponseUtil.ok();
+        else return ResponseUtil.fail(responseCode);
     }
 
 
     @ApiOperation(value = "管理员在广告时段下增加广告", produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "header", dataType = "String", name = "authorization", value = "用户token", required = true),
-            @ApiImplicitParam(paramType = "path", dataType = "String", name = "tid", value = "广告时间段id", required = true),
-            @ApiImplicitParam(paramType = "path", dataType = "String", name = "id", value = "广告id", required = true)
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "did", value = "店ID", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "tid", value = "广告时间段id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Integer", name = "id", value = "广告id", required = true)
 
     })
     @ApiResponses({
@@ -210,7 +243,10 @@ public class AdvertiseController {
             @ApiResponse(code=603, message="达到时段广告上限")
     })
     @PostMapping("/shops/{did}/timesegments/{tid}/advertisement/{id}")
-    public Object createAdvertisementsByTimeSegmentId(@LoginUser Long user, @PathVariable("id") String tid, @PathVariable("id") String id){
-        return null;
+    public Object createAdvertisementsByTimeSegmentIdAndId(@LoginUser Long user, @PathVariable("did") Integer did, @PathVariable("tid") Integer tid, @PathVariable("id") Integer id){
+        ResponseCode responseCode=advertiseService.createAdvertiseByTimeSegIdAndId(id,tid);
+        if(responseCode.equals(ResponseCode.OK))
+            return ResponseUtil.ok();
+        else return ResponseUtil.fail(responseCode);
     }
 }
