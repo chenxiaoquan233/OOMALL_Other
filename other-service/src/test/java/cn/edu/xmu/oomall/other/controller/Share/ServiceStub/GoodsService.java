b48b659b54184768dd056c71b680977dce376e16
yanjiqiang <24320172203229@stu.xmu.edu.cn>
2020-12-15 09:25:57 +0800

    --story=1002516 --user=两吉 测试stub https://www.tapd.cn/54028298/s/1001061

---
+++ other-service/src/test/java/cn/edu/xmu/oomall/other/controller/Share/ServiceStub/GoodsService.java
@@ -0,0 +1,42 @@
+package cn.edu.xmu.oomall.other.controller.Share.ServiceStub;
+
+import cn.xmu.edu.goods.client.IGoodsService;
+import cn.xmu.edu.goods.client.dubbo.OrderItemDTO;
+import cn.xmu.edu.goods.client.dubbo.ShopDTO;
+import cn.xmu.edu.goods.client.dubbo.SkuDTO;
+import cn.xmu.edu.goods.client.dubbo.SpuDTO;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 2 * @author: LiangJi3229
+ * 3 * @date: 2020/12/14 下午7:49
+ * 4
+ */
+public class GoodsService implements IGoodsService {
+    @Override
+    public Long getPrice(Long skuId) {
+        return null;
+    }
+
+    @Override
+    public Map<ShopDTO, List<OrderItemDTO>> classifySku(List<OrderItemDTO> orderItemDTOS) {
+        return null;
+    }
+
+    @Override
+    public SkuDTO getSku(Long skuId) {
+        return null;
+    }
+
+    @Override
+    public SpuDTO getSimpleSpuById(Long spuId) {
+        return null;
+    }
+
+    @Override
+    public ShopDTO getShopBySKUId(Long skuId) {
+        return new ShopDTO(1L);
+    }
+}
---
+++ other-service/src/test/java/cn/edu/xmu/oomall/other/controller/Share/ServiceStub/OrderService.java
@@ -0,0 +1,66 @@
+package cn.edu.xmu.oomall.other.controller.Share.ServiceStub;
+
+import cn.edu.xmu.oomall.dto.AftersaleDTO;
+import cn.edu.xmu.oomall.dto.EffectiveShareDTO;
+import cn.edu.xmu.oomall.dto.ExchangeOrderDto;
+import cn.edu.xmu.oomall.dto.OrderItemDTO;
+import cn.edu.xmu.oomall.impl.IDubboOrderService;
+
+import java.util.List;
+
+/**
+ * 2 * @author: LiangJi3229
+ * 3 * @date: 2020/12/14 下午7:53
+ * 4
+ */
+public class OrderService implements IDubboOrderService {
+    @Override
+    public AftersaleDTO getAfterSaleByOrderItemId(Long orderItemId) {
+        return null;
+    }
+
+    @Override
+    public Boolean isShopOwnOrder(Long shopId, Long orderId) {
+        return null;
+    }
+
+    @Override
+    public Boolean isCustomerOwnOrder(Long customerId, Long orderId) {
+        return null;
+    }
+
+    @Override
+    public Boolean isCustomerOwnOrderItem(Long customerId, Long orderItemId) {
+        return null;
+    }
+
+    @Override
+    public OrderItemDTO getOrderItem(Long orderItemId) {
+        return null;
+    }
+
+    @Override
+    public Boolean orderCanBePaid(Long id) {
+        return null;
+    }
+
+    @Override
+    public void checkOrderPaid(Long id, Long amount) {
+
+    }
+
+    @Override
+    public Long getOrderPresaleDeposit(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<EffectiveShareDTO> getEffectiveShareRecord() {
+        return null;
+    }
+
+    @Override
+    public Integer createExchangeOrder(ExchangeOrderDto dto) {
+        return null;
+    }
+}
