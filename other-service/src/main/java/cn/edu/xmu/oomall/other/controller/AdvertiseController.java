b45822f75eac29fbf0ef09b49968faa47a041fb5
hardworking-qf <1183364194@qq.com>
2020-12-23 14:25:51 +0800

    Revert "--sry=1003015 --user=hwqf 与商品模块通讯服务"
    This reverts commit 62562598

--- other-service/src/main/java/cn/edu/xmu/oomall/other/controller/AdvertiseController.java
+++ other-service/src/main/java/cn/edu/xmu/oomall/other/controller/AdvertiseController.java
@@ -32,7 +32,6 @@ import java.util.List;
  * @author hardworking-qf
  * @version 创建时间：2020/12/12 上午10:44
  */
-//ccc
 @RestController
 @RequestMapping(produces = "application/json;charset=UTF-8")
 public class AdvertiseController {
